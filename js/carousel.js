d12c66349e3b65cdb70fdd72569c705f5ffecfad
Jonathan <cordovaj63@gmail.com>
2024-08-15 10:00:00 -0700

    Added navigation bar styling css as option

--- js/carousel.js
+++ js/carousel.js
@@ -40,17 +40,56 @@
 //     });
 // });
 
+// $(document).ready(function() {
+//     var multipleCardCarousel = document.querySelector("#carouselControls");
+//     if (window.matchMedia("(min-width: 768px)").matches) {
+//         var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+//             interval: false,
+//         });
+//         var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+//         var cardWidth = $(".carousel-item").width();
+//         var scrollPosition = 0;
+
+//         $("#carouselControls .carousel-control-next").on("click", function () {
+//             if (scrollPosition < carouselWidth - cardWidth * 3) {
+//                 scrollPosition += cardWidth;
+//                 $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+//             }
+//         });
+
+//         $("#carouselControls .carousel-control-prev").on("click", function () {
+//             if (scrollPosition > 0) {
+//                 scrollPosition -= cardWidth;
+//                 $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+//             }
+//         });
+//     } else {
+//         $(multipleCardCarousel).addClass("slide");
+//     }
+// });
+
 $(document).ready(function() {
     var multipleCardCarousel = document.querySelector("#carouselControls");
+
+    function updateCarouselDimensions() {
+        var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+        var cardWidth = $(".carousel-item").width();
+        return { carouselWidth, cardWidth };
+    }
+
     if (window.matchMedia("(min-width: 768px)").matches) {
         var carousel = new bootstrap.Carousel(multipleCardCarousel, {
             interval: false,
         });
-        var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-        var cardWidth = $(".carousel-item").width();
+        
+        // Initialize scrollPosition and dimensions
+        var { carouselWidth, cardWidth } = updateCarouselDimensions();
         var scrollPosition = 0;
 
         $("#carouselControls .carousel-control-next").on("click", function () {
+            // Recalculate dimensions to adapt to window resizing
+            var { carouselWidth, cardWidth } = updateCarouselDimensions();
+
             if (scrollPosition < carouselWidth - cardWidth * 3) {
                 scrollPosition += cardWidth;
                 $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
@@ -58,10 +97,19 @@ $(document).ready(function() {
         });
 
         $("#carouselControls .carousel-control-prev").on("click", function () {
+            // Recalculate dimensions to adapt to window resizing
+            var { carouselWidth, cardWidth } = updateCarouselDimensions();
+
             if (scrollPosition > 0) {
                 scrollPosition -= cardWidth;
                 $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
             }
+        });
+
+        // Recalculate dimensions on window resize
+        $(window).on('resize', function() {
+            scrollPosition = 0;  // Reset scroll position
+            $(".carousel-inner").scrollLeft(scrollPosition);  // Reset scroll to start
         });
     } else {
         $(multipleCardCarousel).addClass("slide");
