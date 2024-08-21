028f885b1ac8b5aae48f15961d42a09f96be5369
Jonathan <cordovaj63@gmail.com>
2024-08-21 15:02:30 -0700

    Added variables.js to separate content logic from HTML. Moved project titles, descriptions, and images into variables.js to maintain cleaner code and easier content updates.

---
+++ js/variables.js
@@ -0,0 +1,35 @@
+
+// variables.js
+const firstName = "Jonathan";
+const lastName = "Cordova's";
+const portfolioTitle = `${firstName} ${lastName}\nPortfolio`;  // "Portfolio" on a new line
+const navbarBrandText = "My Name is Jonathan Cordova";
+// const profilePhotoSrc = "../assets/profile_photo.png";
+
+const sectionHomeTitle = "Career Portfolio";
+const sectionHomeDescription = "Description and experience.";
+
+// Project Titles
+const projectTitle1 = "Minimalist To-Do List Android Application";
+const projectTitle2 = "Built a Chess AI using Python";
+const projectTitle3 = "Training Machine Learning Agents to Navigate an Obstacle Course in Unity using C# and Python";
+
+// Project Descriptions
+const projectDescription1 = "This application focuses on providing a simple and intuitive interface for managing daily tasks, "
++ "embracing a minimalist design philosophy to ensure ease of use and a clutter-free experience.";
+
+const projectDescription2 = "Created a Chess AI using Python and Pygame, utilizing the Minimax algorithm with alpha-beta pruning "+
+"for optimal move selection. Supports Player vs. AI mode, demonstrating strategic AI decision-making.";
+
+const projectDescription3 = "This Machine Learning (ML) agent project uses computer vision, 3D ray perception sensors, and reinforcement " +
+"learning to train an agent to navigate obstacles and reach a goal. The agents learns autonomously through trial and error, guided by incremental "
++ "rewards and penalties, within the Unity Game Engine and programmed in C# and Python.";
+
+// Project Images
+// const projectImage1 = "https://via.placeholder.com/150";
+// const projectImage2 = "https://via.placeholder.com/150";
+// const projectImage3 = "https://via.placeholder.com/150";
+
+const projectImage1 = "./assets/projects/Icon_Image_Android_ToDoList.jpg";
+const projectImage2 = "./assets/projects/Icon_Image_Python_ChessAI.jpg";
+const projectImage3 = "./assets/projects/Icon_Image_MLAgent_3.jpg";
