12ce8f3e3f29819e74150ac9620f93f0b07554e1
Jonathan <cordovaj63@gmail.com>
2024-08-23 17:25:20 -0700

    Added Experience Section, restructure section to include separate variables for position and company. Added company icon for each professional experience, added content into the variables.js file.

--- js/variables.js
+++ js/variables.js
@@ -59,3 +59,78 @@ const researchDescription3 = "IEEE Publication 2024";
 const researchImage1 = "./assets/research/icon_image_research_1_energy.png";
 const researchImage2 = "./assets/research/icon_image_research_2_arcs.jpg";
 const researchImage3 = "./assets/research/icon_image_research_3_complexyolo.jpg";
+
+// Experience Section //
+// -------------------------------- //
+
+// Variables for experience 1
+const experienceImgSrc1 = "assets/experience/nasa_jpl_logo.png";
+const experienceImgAlt1 = "NASA Logo";
+const experiencePosition1 = "ARCS Research Fellow";
+const experienceCompany1 = "Autonomy Research Center for STEAHM (ARCS) at California State University, Northridge (CSUN)";
+const experienceDuration1 = "(02/2024 - 05/2024)";
+const experienceDescription1_1 = "Engaged in a NASA-sponsored research fellowship program, a collaborative initiative between CSUN and NASA JPL " 
++ "through the Autonomy Research Center for STEAHM (ARCS).";
+const experienceDescription1_2 = "Led the development of a traffic monitoring system using a projection-based method, " 
++ "Complex YOLO, a machine learning detection and classification model with 92% precision and 86% recall based on our own roadside-collected dataset.";
+const experienceDescription1_3 = "We aimed to enhance transportation management and safety by accurately detecting and classifying vehicles, monitoring "
++ "traffic flow dynamics, and providing real-time insights to optimize highway infrastructure and operations."
+
+// Variables for experience 2
+const experienceImgSrc2 = "assets/experience/arcs_csun_icon.png";
+const experienceImgAlt2 = "Company 2 Logo";
+const experiencePosition2 = "ARCS Research Associate";
+const experienceCompany2 = "Autonomy Research Center for STEAHM (ARCS) at California State University, Northridge (CSUN)";
+const experienceDuration2 = "(08/2023 - 02/2024)";
+const experienceDescription2_1 = "As an ARCS Research Associate at the Autonomy Research Center for STEAHM (ARCS) at California State University, Northridge (CSUN), "
++ "I contributed to a NASA-sponsored project in collaboration with NASA's Jet Propulsion Laboratory (JPL).";
+const experienceDescription2_2 = "I co-led a team of 10 students in developing a machine learning model for real-time vehicle detection and classification using "
++"3D LiDAR point-cloud data.";
+const experienceDescription2_3 = "My role involved managing the team and applying my expertise in machine learning and software engineering to the 3D Object "
++"Detection and Vehicle Classification project aimed at enhancing real-time traffic flow monitoring.";
+
+// Variables for experience 3
+const experienceImgSrc3 = "assets/experience/csun_icon_logo.png";
+const experienceImgAlt3 = "Company 3 Logo";
+const experiencePosition3 = "Research Assistant";
+const experienceCompany3 = "Computer Science Department, California State University, Northridge";
+const experienceDuration3 = "(03/2023 - 12/2023)";
+const experienceDescription3_1 = "Assisted in leading the NSF-sponsored Research Experience for Undergraduates (REU) Program under the supervision of Dr. Xunfei Jiang.";
+const experienceDescription3_2 = "Introduced participants to key technologies and methodologies, including PyTorch, TensorFlow, C language for high-performance "
++ "computing, machine learning, data science, Numpy, Pandas, Jupyter Notebook through Anaconda, Git, GitHub, and Linux/Ubuntu.";
+const experienceDescription3_3 = "Co-authored a publication in IEEE on the practical applications of data science in energy-efficient cluster systems.";
+
+// Variables for experience 4
+const experienceImgSrc4 = "assets/experience/centene-health-net-logo.png";
+const experienceImgAlt4 = "Company 4 Logo";
+const experiencePosition4 = "Business Analyst III";
+const experienceCompany4 = "Centene Corporation - Health Net";
+const experienceDuration4 = "(05/2022 - 08/2022)";
+const experienceDescription4_1 = "Supported business initiatives by conducting data analysis, identifying implementation barriers, and performing user acceptance testing for various systems.";
+const experienceDescription4_2 = "Conducted detailed analysis on multiple projects, recommended potential business solutions, and ensured successful implementations.";
+const experienceDescription4_3 = "Identified methods to enhance performance management and created operational reports related to new business implementation processes.";
+
+// Variables for experience 5
+const experienceImgSrc5 = "assets/experience/centene-health-net-logo.png";
+const experienceImgAlt5 = "Company 5 Logo";
+const experiencePosition5 = "Business Analyst II";
+const experienceCompany5 = "Centene Corporation - Health Net";
+const experienceDuration5 = "(09/2018 - 05/2022)";
+const experienceDescription5_1 = "Provider Data Analytics Team Supporting a provider data system migration project by providing data processing support as a "
++ "workaround solution during the phases of the system migration project.";
+const experienceDescription5_2 = "Testing enhancements, documenting production issues, and creating workaround solutions for system limitations.";
+const experienceDescription5_3 = "Enhancing, maintaining, and troubleshooting VBA compile errors for current system applications including Microsoft Access. " 
++ "Enhancing Flow Charts using Microsoft Visio for Automation testing of SQL Server Business Intelligence SSIS jobs.";
+
+// Variables for experience 6
+const experienceImgSrc6 = "assets/experience/centene-health-net-logo.png";
+const experienceImgAlt6 = "Company 6 Logo";
+const experiencePosition6 = "Data Coordinator";
+const experienceCompany6 = "Centene Corporation - Health Net";
+const experienceDuration6 = "(08/2017 - 09/2018)";
+const experienceDescription6_1 = "Running SQL queries to pull paid claim and capitation data to report and audit the completeness and accuracy of the data for the CalPERS and "
++ "Health Net Legacy Employees Groups.";
+const experienceDescription6_2 = "Successfully managing the reporting of weekly and monthly paid claims for the Health Net Centene Corporate Legacy Employee "
++ "account using Microsoft Excel PV Tables, Vlookups, Functions, Visual Basic Application (VBA) Macros for claims including Medical, Mental Health and Pharmacy.";
+const experienceDescription6_3 = "Successfully manage the Health Net Centene's client account, CalPERS, including the reconciling, billing, and reporting for "
++ "capitation, claims, and administrative fees using Excel Visual Basic (VBA) Macros and VBA programming techniques to automate procedural processes.";
